fae729b8e44d9c11fffc91adf45989540138d58a
hertz315 <hertz315@gmail.com>
2022-12-18 21:45:56 +0900

    Initial Commit

---
+++ SearchTattoo/SearchTattooApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchTattooApp.swift
+//  SearchTattoo
+//
+//  Created by Hertz on 12/18/22.
+//
+
+import SwiftUI
+
+@main
+struct SearchTattooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
